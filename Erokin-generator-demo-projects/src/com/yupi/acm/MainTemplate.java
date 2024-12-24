a938c7d232b5ebaf3ef7dfcce49d36238eddf18f
kinzuki <15068859+kinzuki@user.noreply.gitee.com>
2024-12-24 12:16:43 +0800

    项目初始化

---
+++ Erokin-generator-demo-projects/src/com/yupi/acm/MainTemplate.java
@@ -0,0 +1,26 @@
+package com.yupi.acm;
+
+import java.util.Scanner;
+
+public class MainTemplate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextInt()){
+            //读取输入元素个数
+            int n = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+            //处理问题逻辑，根据需要进行输出
+            //示例：计算数组元素之和
+            int sum = 0;
+            for (int num:arr) {
+                sum += num;
+            }
+            System.out.println("Sum：" + sum);
+        }
+        scanner.close();
+
+    }
+}
