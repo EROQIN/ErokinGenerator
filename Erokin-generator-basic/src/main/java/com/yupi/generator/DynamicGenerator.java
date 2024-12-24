cfd7f933eca418cf703d780e07f75bba5496004d
kinzuki <15068859+kinzuki@user.noreply.gitee.com>
2024-12-24 23:20:11 +0800

    使用动态引擎生成动态文件-方法提取

--- Erokin-generator-basic/src/main/java/com/yupi/generator/DynamicGenerator.java
+++ Erokin-generator-basic/src/main/java/com/yupi/generator/DynamicGenerator.java
@@ -16,31 +16,16 @@ import java.io.Writer;
  */
 public class DynamicGenerator {
     public static void main(String[] args) throws IOException, TemplateException {
-        //------------FreeMarker----------------
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
-        cfg.setNumberFormat("0.######");  // now it will print 1000000
-        // 模板文件所在的目录
-        String templatePath = "Erokin-generator-basic"+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"template";
-        cfg.setDirectoryForTemplateLoading(new File(templatePath));
-
-        //字符集设置
-        cfg.setDefaultEncoding("UTF-8");
-        //错误信息输出设置
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        //获取模板
-        Template template = cfg.getTemplate("MainTemplate.java.ftl");
-        //指定输出流流向的文件,创建流管道
-        Writer out = new FileWriter("MainTemplate.java");
+        //获取模板文件路径：
+        String inputPath = "Erokin-generator-basic\\src\\main\\resources\\template\\MainTemplate.java.ftl";
+        String outputPath = "Erokin-generator-basic\\src\\main\\resources\\MainTemplate.java";
         //创建数据填充模型
         MainTemplateConfig dataModel = new MainTemplateConfig();
         dataModel.setAuthor("Kinzuki");
         dataModel.setOutputText("Hello World");
         dataModel.setLoop(true);
-        //最终执行！
-        template.process(dataModel, out);
-        //善后工作
-        out.close();
 
+        doGenerate(inputPath, outputPath, dataModel);
     }
 
 
@@ -48,39 +33,41 @@ public class DynamicGenerator {
 
     /**
      * 通过FreeMarker生成文件
-     * @param templatePath 模板路径(相对于template目录下的)
+     * @param inputPath 模板路径
      * @param outputPath 输出路径
      * @param dataModel 填充数据
      */
-    public void GnerateFileByFreemarker(String templatePath, String outputPath,Object dataModel) throws IOException, TemplateException {
+    public static void doGenerate(String inputPath, String outputPath, Object dataModel) {
         //------------FreeMarker----------------
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
         cfg.setNumberFormat("0.######");  // now it will print 1000000
-        // 文件所在的目录
-        cfg.setDirectoryForTemplateLoading(new File("src/main/resources/template"));
+        // 模板文件所在的目录
+        File templateDir = new File(inputPath).getParentFile();
+
+        try {
+            cfg.setDirectoryForTemplateLoading(templateDir);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         //字符集设置
         cfg.setDefaultEncoding("UTF-8");
         //错误信息输出设置
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        //获取模板
-        Template template = cfg.getTemplate("test.txt.ftl");
-        //指定输出流流向的文件,创建流管道
-        Writer out = new FileWriter("test.txt");
-        //创建填充数据模型
-//        HashMap<String, Object> dataModel = new HashMap<>();
-//
-//        dataModel.put("name", "World");
-//        dataModel.put("age", 180000);
-//        dataModel.put("author", "Kinzuki");
-//
-//        List<String> list = new ArrayList<>();
-//        list.add("1");
-//        list.add("2da");
-//        list.add("3");
-//        dataModel.put("menuItems",list);
-        //模板+填充数据=>输出文件
-        template.process(dataModel, out);
-        //关闭流
-        out.close();
+        try {
+            String templateName = new File(inputPath).getName();
+            //获取模板
+            Template template = cfg.getTemplate(templateName);
+            //指定输出流流向的文件,创建流管道
+            Writer out = new FileWriter(outputPath);
+            //最终执行！
+            template.process(dataModel, out);
+            //善后工作
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (TemplateException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
