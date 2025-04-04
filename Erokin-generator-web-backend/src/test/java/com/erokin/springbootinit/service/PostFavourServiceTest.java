abc1269dc737aa203554f4a63b2b5ef0016bd7e1
Erokin <1107891366@qq.com>
2025-04-04 19:57:32 +0800

    云平台开发阶段准备

---
+++ Erokin-generator-web-backend/src/test/java/com/erokin/springbootinit/service/PostFavourServiceTest.java
@@ -0,0 +1,30 @@
+package com.erokin.springbootinit.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.erokin.generator.model.entity.Post;
+import com.erokin.generator.model.entity.User;
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 帖子收藏服务测试
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class PostFavourServiceTest {
+
+
+    private static final User loginUser = new User();
+
+    @BeforeAll
+    static void setUp() {
+        loginUser.setId(1L);
+    }
+
+}
